11fa3ee1704ba0eda893aeff5632dbf981375f89
pitaz <peter.odekwo@gmail.com>
2021-08-26 09:17:10 +0100

    add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
+import { ButtonProps } from "./interfaces";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+const CustomButton: FC<ButtonProps> = ({title}) => {
+  const classes = useStyles();
+  return (
+    <Button
+      variant="contained"
+      color="secondary"
+      className={classes.button}
+      // startIcon={<DeleteIcon />}
+    >
+      {title}
+    </Button>
+  );
+};
+
+export default CustomButton;
